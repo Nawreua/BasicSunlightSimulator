efd837e05ee830485373f9f43c854701c321c759
Nawreua <nawreua@gmail.com>
2025-11-09 23:07:09 +0100

    sunlight: add sunlight color gradient

--- entities/sun/sun.gd
+++ entities/sun/sun.gd
@@ -3,8 +3,10 @@ extends DirectionalLight3D
 @export var distance: float = 1000 # The distance between the sun sprite and the ground
 @export var rotation_degree: float = -15 # The degree of rotation of the sun each second
 @export var color: Color = Color(1,1,1,1)
+@export var gradient: Gradient
 
 @export var render_3D: bool = true # Select wich sun object to render
+@export var color_gradient: bool = true
 
 # The approximate ratio scale of the sun size over the sun-earth distance
 var distance_to_size_scale: float = 0.009300934521924315
@@ -18,4 +20,7 @@ func _ready() -> void:
 	light_color = color
 
 func _physics_process(delta: float) -> void:
-	rotate_x(deg_to_rad(rotation_degree * delta))
+	var rotation_delta = rotation_degree * delta
+	rotate_x(deg_to_rad(rotation_delta))
+	if color_gradient:
+		light_color = gradient.sample(abs(rotation.x) / PI)
